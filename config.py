b8fb7c3bfbfd17364179ba7a107188c87c903452
Maalug Jonathan <jmaalug11@gmail.com>
2024-09-01 19:36:22 +0000

    updated the database url for render

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://nyameget:17cvsOhOqdY8Kc6ylEi2KNMyioBE3I8h@dpg-cqln8dlumphs7397crmg-a.oregon-postgres.render.com/dbname_ksxs')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://nyameget:xU9xKSe8zsSaeLqrUeotCAsdWNTzSwXX@dpg-cr938hq3esus73bfgb7g-a.oregon-postgres.render.com/dbname_skju')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_KEY = "NVOQ06FFhwbHBjlEi54WjwcJA2wXKL2w5ygGyuGVxrfQ1MAbkF"
     IDENTIFICATION_URL = "https://plant.id/api/v3/identification?details=common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods&language=en"
